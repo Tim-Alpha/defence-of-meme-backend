8f030563f412952e147f34e6d7cf27ad98530a07
Tim-Alpha <codewithaisha@gmail.com>
2024-05-25 01:36:37 +0530

    update: remove comments

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Create a MySQL connection
 const db = mysql.createConnection({
   host: 'bajyst8d3ii2pggur7nf-mysql.services.clever-cloud.com',
   user: 'u9vcahinm9sf2rwt',
@@ -22,7 +21,6 @@ db.connect((err) => {
   console.log('Connected to database');
 });
 
-// Endpoint to save score and hash
 app.post('/save-score', (req, res) => {
   const { hash, score } = req.body;
   const query = 'INSERT INTO scores (hash, score) VALUES (?, ?)';
@@ -35,7 +33,6 @@ app.post('/save-score', (req, res) => {
   });
 });
 
-// Retrieve score by hash and include hash in the response
 app.get('/score', (req, res) => {
   const hash = req.query.hash;
   const query = 'SELECT score FROM scores WHERE hash = ?';
@@ -45,7 +42,6 @@ app.get('/score', (req, res) => {
       return res.status(500).send('Error retrieving data');
     }
     if (results.length > 0) {
-      // Send both score and hash back in the response
       res.status(200).json({ hash, score: results[0].score });
     } else {
       res.status(404).send('No data found');
